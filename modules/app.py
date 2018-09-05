afde1b0b233eda9a743d7572fdc01bc7c00426e8
Nageshpermand <Nageshpermand>
2018-09-05 22:58:41 +0530

    modules and database

---
+++ modules/app.py
@@ -0,0 +1,20 @@
+'''
+b=20
+print a
+'''
+'''
+print "program started"
+b=20
+import sales
+print b
+print "program ended"
+'''
+'''
+import sales
+print a
+'''
+b=20
+import sales
+print b
+print sales.a
+sales.fun()
